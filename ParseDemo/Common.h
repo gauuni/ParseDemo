790e44366b0b4c977e6518bd0ba3ea663baee738
KhoiNguyen <gauuniios@icloud.com>
2015-12-19 11:01:23 +0700

    Add AFNetworking

---
+++ ParseDemo/Common.h
@@ -0,0 +1,17 @@
+//
+//  Common.h
+//  ParseDemo
+//
+//  Created by Nguyen Khoi Nguyen on 12/19/15.
+//  Copyright © 2015 Nguyen Khoi Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Common : NSObject
++ (UIImage*)compressImage:(UIImage*)image;
+
++(UIImage*)scaleImage: (UIImage*) sourceImage;
++(NSString *)uuidString;
+@end
